5796d32728d535b9d4b6180312e960afe84f62e2
Kiratech <account-alpitour@kiratech.it>
2024-11-19 10:51:13 +0100

    change color and add image

--- sript.js
+++ sript.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Page loaded');
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', () => {
+        alert('Hai cliccato il pulsante!');
+    });
 });

0fe8f3996381d8234200924817655cc60eb68e3d
stefanoc1996 <133640296+stefanoc1996@users.noreply.github.com>
2025-03-15 00:03:00 +0100

    Co-authored-by: Roberto Cito <RMChannel@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,52 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Inizializza mappa
-    const map = L.map('map').setView([42.14944, 11.93806], 17);
-    
-// Usa un layer alternativo con più dettagli
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap',
-    maxZoom: 25,
-    minZoom: 15
-}).addTo(map);
+  function initMap() {
+      // Inizializza la mappa con il centro predefinito
+      var center = { lat: 42.14944, lng: 11.93806 };
+      var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: center,
+          mapTypeId: 'roadmap'
+      });
 
-    // Punti di interesse
-    const points = [
-        { 
-            name: "TEATRO CLAUDIO", 
-            coords: [42.15025, 11.93487], 
-            info: "Via Teatro Claudio, 1",
-            icon: L.icon({
-                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
-                iconSize: [25, 41]
-            })
-        },
-        {
-            name: "LA ROCCA",
-            coords: [42.15214183489226, 11.944040910485032],
-            info: "Fortezza medievale"
-        },
-        {
-            name: "COMUNE",
-            coords: [42.149440768633525, 11.937874143985388],
-            info: "Municipio di Tolfa"
-        }
-    ];
+      // Punti di interesse
+      const points = [
+          { 
+              name: "TEATRO CLAUDIO", 
+              coords: { lat: 42.15025, lng: 11.93487 }, 
+              info: "Via Teatro Claudio, 1"
+          },
+          {
+              name: "LA ROCCA",
+              coords: { lat: 42.15214183489226, lng: 11.944040910485032 },
+              info: "Fortezza medievale"
+          },
+          {
+              name: "COMUNE",
+              coords: { lat: 42.149440768633525, lng: 11.937874143985388 },
+              info: "Municipio di Tolfa"
+          }
+      ];
 
-    // Aggiungi marker
-    points.forEach(point => {
-        L.marker(point.coords, {
-            opacity: 0.9,
-            title: point.name
-        }).addTo(map)
-        .bindPopup(`<div class="map-popup">
-                      <h6>${point.name}</h6>
-                      <p>${point.info}</p>
-                    </div>`);
-    });
+      // Aggiungi marker
+      points.forEach(point => {
+          var marker = new google.maps.Marker({
+              position: point.coords,
+              map: map,
+              title: point.name
+          });
 
-    L.control.scale({ imperial: false }).addTo(map)
-    // Adatta la mappa al resize
-    window.addEventListener('resize', () => map.invalidateSize());
+          var infoWindow = new google.maps.InfoWindow({
+              content: `<div class="map-popup">
+                            <h6>${point.name}</h6>
+                            <p>${point.info}</p>
+                        </div>`
+          });
+
+          marker.addListener("click", function() {
+              infoWindow.open(map, marker);
+          });
+      });
+  }
+
+  window.initMap = initMap;
+
+
+  
     fetch('programma.json')
     .then(response => response.json())
     .then(data => {
